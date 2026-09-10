4c65d6f8d2645a1657fc7c9dd53494cd60771a72
saranciucnichita <saranciucnichita@gmail.com>
2026-09-10 10:25:49 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  allowedDevOrigins: ['127.0.0.1'],
+};
+
+export default nextConfig;
